5a76d370df7f3b40764a82e79c590474a5766782
Zyavon69 <sbilous045@gmail.com>
2026-02-10 10:40:51 +0300

    Second commit

--- main.c
+++ main.c
@@ -5,12 +5,12 @@ int main() {
     printf("Hello, world %d\n", a);
     int* b;
     b = &a;
-     *b += 10;
-    int nums[] = {1,2,3,4,5};
-    int* u;
-    u = &nums;
-    *u = nums[0];
-    printf("%p\n",u);
-    *u = nums[1];
-    printf("%p",u);
+    *b += 10;
+    while (1) {
+        int nums[] = {1,2,3,4,5};
+        int* u;
+        u = &nums;
+        *u = nums[0];
+        printf("%p\n",u);
+    }
 }
