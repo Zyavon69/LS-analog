4ba20cbca83c33b762894efd8de5da2ea775fe86
Zyavon69 <sbilous045@gmail.com>
2026-02-23 09:51:15 +0300

    Concatinate string

--- main.c
+++ main.c
@@ -3,19 +3,28 @@
 #include <string.h>
 
 int main() {
-    char str1[40] = "Hello, duo";
-    char str2[10] = " help";
+    char str1[100] = "Hello, duo";
+    char str2[10] = " help ";
 
     unsigned long nullterm = strlen(str1);
 
-    for (int i =0; i < 40; i ++) {
-        if (str1[i] == '\0') {
-            str1[i] = str2[0 + i - nullterm ];
+    for (int i =0; i < sizeof(str1); i ++) {
+        if (nullterm + strlen(str2 + 1) > sizeof(str1)) {
+            printf("Строка выходит за размер массива");
+            break;
+        } else {
+            if (str1[i] == '\0') {
+                str1[i] = str2[0 + i - nullterm ];
+                if (nullterm + strlen(str2 + 1) <= i) {
+                    break;
+                }
+            }
         }
     }
 
-    for (int i = 0; i < 40; i++) {
-        printf("%c",(int)str1[i]);
+    
+    for (int i =0; i < 100; i ++) {
+        printf("%c", str1[i]);
     }
 }
 
