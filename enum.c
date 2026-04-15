4098ed269226b41e9198cfab2a15fe6e24e92d86
Zyavon69 <sbilous045@gmail.com>
2026-04-15 16:13:07 +0300

    Eight commit

--- enum.c
+++ enum.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <string.h>
 enum Word_Types {
@@ -9,33 +10,42 @@ struct Token {
     char *value;
 };
 
+struct Token_Array {
+    struct Token arr[100];
+};
+
+
 int main() {
     char str[10] = "Walk";
-    char *pstr = &str[0];
+    char str1[10] = "Go";
+    char *string[100] = {&str[0],&str1[0]};
+    for (int i = 0; i < 2; i ++) {
+        for (int j = 0; j < 10; j ++)
+        printf("%c\n", string[i][j]);
+    }
+}
+
+/*void tokenize(struct Token_Array *array_tokens, char *pstr) {
     enum Word_Types adj = Adj;
     enum Word_Types sub = Sub;
     enum Word_Types verb = Verb;
     enum Word_Types noun = Noun;
     enum Word_Types nul = NUL;
 
-    struct Token token;
-
     if (strcmp(pstr, "Beautiful") == 0 || strcmp(pstr, "Ugly") == 0 || strcmp(pstr, "Obsessive") == 0) {
-        token.tokentypes = adj;
-        token.value = pstr;
+        array_tokens->arr[0].tokentypes = adj;
+        array_tokens->arr[0].value = pstr;
     } else if (strcmp(pstr, "I") == 0 || strcmp(pstr, "You") == 0 || strcmp(pstr, "We") == 0 ) {
-        token.tokentypes = sub;
-        token.value = pstr;
+        array_tokens->arr[1].tokentypes = sub;
+        array_tokens->arr[1].value = pstr;
     } else if (strcmp(pstr, "Take") == 0 || strcmp(pstr, "Sit") == 0 || strcmp(pstr, "Walk") == 0 ) {
-        token.tokentypes = verb;
-        token.value = pstr;
+        array_tokens->arr[0].tokentypes = adj;
+        array_tokens->arr[0].value = pstr;
     } else if (strcmp(pstr, "Tragg") == 0 || strcmp(pstr, "Dog") == 0 || strcmp(pstr, "Cat") == 0 ) {
         token.tokentypes = noun;
         token.value = pstr;
     } else  {
         token.tokentypes = nul;
         token.value = pstr;
-    }
-
-    printf("%d", token.tokentypes);
-}
+    }    
+}*/
